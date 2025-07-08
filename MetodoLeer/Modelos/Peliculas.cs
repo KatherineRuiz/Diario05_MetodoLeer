23041e62e198d3defcc70a3ffda17be83f935bd7
Katherine <20250409@ricaldone.edu.sv>
2025-07-07 21:04:05 -0600

    CRUD

---
+++ MetodoLeer/Modelos/Peliculas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+//Agregamos la librería para conectarnos a SQL Server
+
+namespace Modelos
+{
+    public class Peliculas
+    {
+        private int id;
+        private string nombre;
+        private string director;
+        private DateTime fechaLanzamiento;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Director { get => director; set => director = value; }
+        public DateTime FechaLanzamiento { get => fechaLanzamiento; set => fechaLanzamiento = value; }
+
+        //Creamos un método estatico DataTable para poder cargar los registros que estan en la base de datos en una tabla y mostrarla
+        public static DataTable CargarPeliculas() 
+        { 
+            //  Creamos una variable de tipo SqlConnection
+            SqlConnection con = Conexion.Conectar();
+            //Llamam0s al objeto que ya creamos en la clases Conexion
+            string comando = "select * from peliculas;";
+
+            //Creamos un objeto de SqlAdapter para poder obtener el resultado de nuestro select
+            //Enviamos 2 parametros al constructor, uno donde se encuentre el comando de Sql y el segunod, la variable de tipo conexion
+            SqlDataAdapter ad = new SqlDataAdapter(comando, con);
+            //Ahora, creamos un objeto de tipo DataTable. Esta es una tabla en la que guardaremos la información que viene de SQL Server
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            //Ahora, "vertimos" o "echamos" la información del adaptador en la tabla
+            return dt;
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+create database MetodoLeer
+go
+use MotodoLeer;
+go
+
+create table Peliculas(
+id int primary key identity (1,1),
+nombre varchar(50) not null,
+director varchar(50) not null,
+fechaLanzamiento date
+);
+
+insert into Peliculas(nombre, director, fechaLanzamiento) values 
+('The Shawshank Redemption', 'Frank Darabont', '1994/09/23'),
+('The Godfather', 'Francis Ford Coppola', '1972/03/24'),
+('Pulp Fiction', 'Quentin Tarantino', '1994/10/14'),
+('The Dark Knight', 'Christopher Nolan', '2008/07/18'),
+('Fight Club', 'David Fincher', '1999/10/15')
+;
+
+select * from Peliculas
